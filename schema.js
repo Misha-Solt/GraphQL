792f6dbb2f8026f9d2d9089f7933a7b40b3c101e
Misha-Solt <mikhail.us@gmail.com>
2024-04-07 21:56:18 +0200

    Related data added

--- schema.js
+++ schema.js
@@ -1,25 +1,30 @@
-
-
 export const typeDefs = `#graphql
 type Game {
     id: ID!
     title: String!
     platform:[String!]!
+    reviews: [Review!]
 }
 type Review {
     id: ID!
     rating: Int!
     content: String!
+    author: Author!
+    game: Game!
 }
 type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
 }
 type Query{
-    reviews: [Review]
     games: [Game]
+    game(id: ID!): Game
+    reviews: [Review]
+    review(id: ID!): Review
     authors: [Author]
+    author(id: ID!): Author
 }
 `
 
